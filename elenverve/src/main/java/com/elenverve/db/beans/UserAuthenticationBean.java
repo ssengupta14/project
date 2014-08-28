e2de5878a11e585cd9e86506b253f588e7096508
Vinod <vinod.m.shiv@gmail.com>
2014-08-27 21:13:40 -0400

    login

---
+++ elenverve/src/main/java/com/elenverve/db/beans/UserAuthenticationBean.java
@@ -0,0 +1,44 @@
+package com.elenverve.db.beans;
+
+public class UserAuthenticationBean {
+
+	private String emailId;
+	private String password;
+	private String authority;
+	/**
+	 * @return the emailId
+	 */
+	public String getEmailId() {
+		return emailId;
+	}
+	/**
+	 * @param emailId the emailId to set
+	 */
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the authority
+	 */
+	public String getAuthority() {
+		return authority;
+	}
+	/**
+	 * @param authority the authority to set
+	 */
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+}
