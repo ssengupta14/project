b54d7c3eb93cd414798bed92e1edabed93b44d59
ssengupta14 <ssengupta14@gmail.com>
2014-10-07 22:58:57 -0400

    adding customer to session

--- elenverve/src/main/java/com/elenverve/controller/DefaultController.java
+++ elenverve/src/main/java/com/elenverve/controller/DefaultController.java
@@ -1,14 +1,20 @@
 package com.elenverve.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.elenverve.dao.DbAccessService;
 
 @Controller
+@RequestMapping("/")
 public class DefaultController {
 	//@Autowired
 	protected DbAccessService dbAccess= new DbAccessService();
-
+	
 	
 	
 
