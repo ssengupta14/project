12c1055088c42a77efe5f2cd82b7cbd6d7cc5126
ssengupta14 <ssengupta14@gmail.com>
2014-10-11 01:13:14 -0400

    Added additional fields

--- elenverve/src/main/java/com/elenverve/dvo/SocialUserDvo.java
+++ elenverve/src/main/java/com/elenverve/dvo/SocialUserDvo.java
@@ -1,9 +1,32 @@
 package com.elenverve.dvo;
 
-public abstract class SocialUserDvo {
+public abstract class SocialUserDvo extends CustomerDvo{
 
 	
 	private String username;
+	private String displayName;
+	public String getDisplayName() {
+		return displayName;
+	}
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+	public String getProfileUrl() {
+		return profileUrl;
+	}
+	public void setProfileUrl(String profileUrl) {
+		this.profileUrl = profileUrl;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	private String profileUrl;
+	private String imageUrl;
+	
 	
 	private int type = Integer.MIN_VALUE;
 	
